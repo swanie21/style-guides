fdddb6f0a2c25dd3334dae438674a71a4d0c6e75
swanie21 <kswanie21@gmail.com>
2016-10-02 13:42:08 -0600

    change project order again and fix js buttons

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,54 @@
 'use strict';
 
-function hideCodeSnippet() {
-  $('.code-snippet').hide();
+function hideCodeSnippetOne() {
+  $('#code-snippet-one').hide();
 }
 
-$('.show-code-button').on('click', function() {
-  $('.code-snippet').show();
+function hideCodeSnippetTwo() {
+  $('#code-snippet-two').hide();
+}
+
+function hideCodeSnippetThree() {
+  $('#code-snippet-three').hide();
+}
+
+function hideCodeSnippetFour() {
+  $('#code-snippet-four').hide();
+}
+
+$('#show-code-button-one').on('click', function() {
+  $('#code-snippet-one').show();
 });
 
-$('.code-snippet').on('mouseleave', function() {
-  hideCodeSnippet();
+$('#code-snippet-one').on('mouseleave', function() {
+  hideCodeSnippetOne();
 });
 
-hideCodeSnippet();
+$('#show-code-button-two').on('click', function() {
+  $('#code-snippet-two').show();
+});
+
+$('#code-snippet-two').on('mouseleave', function() {
+  hideCodeSnippetTwo();
+});
+
+$('#show-code-button-three').on('click', function() {
+  $('#code-snippet-three').show();
+});
+
+$('#code-snippet-three').on('mouseleave', function() {
+  hideCodeSnippetThree();
+});
+
+$('#show-code-button-four').on('click', function() {
+  $('#code-snippet-four').show();
+});
+
+$('#code-snippet-four').on('mouseleave', function() {
+  hideCodeSnippetFour();
+});
+
+hideCodeSnippetOne();
+hideCodeSnippetTwo();
+hideCodeSnippetThree();
+hideCodeSnippetFour();
